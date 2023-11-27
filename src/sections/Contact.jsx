51af7b9d16de339d523372dd4c47ab82fa678510
Praveenraj002 <praveenbose264@gmail.com>
2023-11-27 14:00:58 +0530

    added changes

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -17,21 +17,23 @@ const Contact = () => {
               Let&apos;s <span className="text-atlassianBlue">Connect</span>
             </SectionHeader>
           </div>
-          <div className="text-center my-2 ">
+          <div className="text-center my-2">
             <p className="mb-3">
               I&apos;m excited to connect with others to chat about projects and
               ideas.
             </p>
             <h2
-              className="relative text-sm md:text-lg text-center font-poppins font-semibold text-green-600 bg-green-300 rounded-md py-2 px-4 flex flex-row items-center justify-center"
+              className="relative text-sm md:text-lg text-center font-poppins font-semibold text-green-600 bg-green-300 rounded-md py-2 px-4 block"
               data-aos="flip-up"
             >
-              Employment Status:{" "}
-              {/* <span>
-                <GoDotFill />
-              </span> */}
               Available For work
             </h2>
+            {/* <h2
+              className="relative text-sm md:text-lg text-center font-poppins font-semibold text-red-600 bg-red-300 rounded-md py-2 px-4 block"
+              data-aos="flip-up"
+            >
+              Unavailable for work
+            </h2> */}
           </div>
         </div>
         <div
